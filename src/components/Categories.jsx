6d93e4254cf82d5513a56f3fcd343139baeeb016
Maryam Bouchami <maryambouchami@gmail.com>
2026-02-24 21:32:19 +0100

    make it responsive

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,19 +4,20 @@ import questionsHist from "../data/quiz-history.json";
 import questionPsy from "../data/quiz-psychology.json";
 import questionsScience from "../data/quiz-science.json";
 import styles from "../styles/category.module.css";
+
 export default function Categories({ setQuestions }) {
   const navigate = useNavigate();
 
   const handleCategory = (questionsData) => {
-    setQuestions(questionsData); // Met à jour l'état dans Homepage
-    navigate("/quiz", { state: { questions: questionsData } }); // Va sur /quiz
+    setQuestions(questionsData);
+    navigate("/quiz", { state: { questions: questionsData } });
   };
 
   return (
     <div className={styles.quizCategories}>
-      <button onClick={() => handleCategory(questionsLitt)}>Litterature</button>
-      <button onClick={() => handleCategory(questionsHist)}>History</button>
-      <button onClick={() => handleCategory(questionPsy)}>Psychology</button>
+      <button onClick={() => handleCategory(questionsLitt)}>Littérature</button>
+      <button onClick={() => handleCategory(questionsHist)}>Histoire</button>
+      <button onClick={() => handleCategory(questionPsy)}>Psychologie</button>
       <button onClick={() => handleCategory(questionsScience)}>Science</button>
     </div>
   );
