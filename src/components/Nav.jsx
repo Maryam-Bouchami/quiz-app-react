dc48a8f0cb2e645fbb6586172c6280babef82291
Maryam Bouchami <maryambouchami@gmail.com>
2026-02-24 21:49:53 +0100

    make navbar responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,13 +3,13 @@ import styles from "../styles/nav.module.css";
 
 export default function Nav() {
   return (
-    <div className={styles.navbar}>
-      <Link className={styles.Link} to="/">
+    <nav className={styles.navbar}>
+      <Link to="/" className={styles.Link}>
         <div className={styles.logo}>
-          <img src="/quiz.png" alt="Quiz Logo" />
-          <h1>Quiz App With React</h1>
+          <img src="/quiz.png" alt="Quiz App Logo" />
+          <h1>Quiz App</h1>
         </div>
       </Link>
-    </div>
+    </nav>
   );
 }
